7d918c718039e99f12b287c95f6247832b512d67
JunMock <kjmzzang01@gmail.com>
2020-01-21 11:33:46 +0900

    make faq

--- violet/src/router/index.js
+++ violet/src/router/index.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import About from "../views/About.vue";
-
+import Faq from "../views/Faq.vue";
 Vue.use(VueRouter);
 
 const routes = [
@@ -15,6 +15,11 @@ const routes = [
     path: "/about",
     name: "about",
     component: About
+  },
+  {
+    path: "/faq",
+    name: "faq",
+    component: Faq
   }
 ];
 
