01e5638d7d2a0609e944b46d9d46116644b63c62
정구헌 <honeion2012@gmail.com>
2020-02-03 15:53:51 +0900

    Back - modify dto

--- back-spring/src/main/java/com/pjt1/demo/model/dto/Files.java
+++ back-spring/src/main/java/com/pjt1/demo/model/dto/Files.java
@@ -12,21 +12,76 @@ import lombok.Setter;
 import lombok.ToString;
 
 //이미지 파일을 위한 테이블
-@NoArgsConstructor
-@RequiredArgsConstructor
-@AllArgsConstructor
-@ToString(exclude="serialVersionUID")
-@EqualsAndHashCode
 public class Files implements Serializable {
 	
 	private static final long serialVersionUID = 7763176721893127276L;
-	@Getter private int files_no 			;//int NOT NULL KEY AUTO_INCREMENT,
-	@Getter @Setter @NonNull private int post_no 			;//int NOT NULL,
-	@Getter @Setter @NonNull private String files_name		;//varchar(200) NOT NULL,
-	@Getter @Setter private boolean files_thumbnail ;//boolean,
-	@Getter @Setter @NonNull private String files_url 		;//varchar(500) NOT NULL, #PATH
-	@Getter @Setter private Boolean files_del_check ;//DEFAULT FAlSE, 			# 파일 삭제 여부
+	private int files_no 			;//int NOT NULL KEY AUTO_INCREMENT,
+	private int post_no 			;//int NOT NULL,
+	private String files_name		;//varchar(200) NOT NULL,
+	private boolean files_thumbnail ;//boolean,
+	private String files_url 		;//varchar(500) NOT NULL, #PATH
+	private Boolean files_del_check ;//DEFAULT FAlSE, 			# 파일 삭제 여부
 //	FOREIGN KEY(post_no) REFERENCES POST(post_no)
-	
+	public Files() {
+		super();
+	}
+	public Files(int post_no, String files_name, boolean files_thumbnail, String files_url) {
+		super();
+		this.post_no = post_no;
+		this.files_name = files_name;
+		this.files_thumbnail = files_thumbnail;
+		this.files_url = files_url;
+	}
+	public Files(int files_no, int post_no, String files_name, boolean files_thumbnail, String files_url,
+			Boolean files_del_check) {
+		super();
+		this.files_no = files_no;
+		this.post_no = post_no;
+		this.files_name = files_name;
+		this.files_thumbnail = files_thumbnail;
+		this.files_url = files_url;
+		this.files_del_check = files_del_check;
+	}
+	@Override
+	public String toString() {
+		return "Files [files_no=" + files_no + ", post_no=" + post_no + ", files_name=" + files_name
+				+ ", files_thumbnail=" + files_thumbnail + ", files_url=" + files_url + ", files_del_check="
+				+ files_del_check + "]";
+	}
+	public int getPost_no() {
+		return post_no;
+	}
+	public void setPost_no(int post_no) {
+		this.post_no = post_no;
+	}
+	public String getFiles_name() {
+		return files_name;
+	}
+	public void setFiles_name(String files_name) {
+		this.files_name = files_name;
+	}
+	public boolean isFiles_thumbnail() {
+		return files_thumbnail;
+	}
+	public void setFiles_thumbnail(boolean files_thumbnail) {
+		this.files_thumbnail = files_thumbnail;
+	}
+	public String getFiles_url() {
+		return files_url;
+	}
+	public void setFiles_url(String files_url) {
+		this.files_url = files_url;
+	}
+	public Boolean getFiles_del_check() {
+		return files_del_check;
+	}
+	public void setFiles_del_check(Boolean files_del_check) {
+		this.files_del_check = files_del_check;
+	}
+	public int getFiles_no() {
+		return files_no;
+	}
+		
+		
 	
 }
--- back-spring/src/main/java/com/pjt1/demo/model/service/FaqServiceImpl.java
+++ back-spring/src/main/java/com/pjt1/demo/model/service/FaqServiceImpl.java
@@ -8,31 +8,47 @@ import org.springframework.stereotype.Service;
 import com.pjt1.demo.model.dto.Faq;
 
 import lombok.SneakyThrows;
+
 @Service
 public class FaqServiceImpl implements FaqService {
 
 	@Autowired
 	private FaqService repo;
+	
+	@Override 
 	@SneakyThrows(Exception.class)
 	public Faq search(int faq_no) {
 		return repo.search(faq_no);
 	}
 
-	@SneakyThrows(Exception.class)
+	@Override
+//	@SneakyThrows(Exception.class)
 	public List<Faq> searchAll() {
-		return repo.searchAll();
+		try {
+		System.out.println("zzzzzzzzzzzzzzzzzzzzzzzz");
+		List<Faq> faqList = repo.searchAll();
+		System.out.println("########");
+		System.out.println(faqList);
+		return faqList;
+		}catch(Exception e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
-
+	
+	@Override
 	@SneakyThrows(Exception.class)
 	public void insert(Faq Faq) {
 		repo.insert(Faq);
 	}
-
+	
+	@Override
 	@SneakyThrows(Exception.class)
 	public void update(Faq Faq) {
 		repo.update(Faq);
 	}
-
+	
+	@Override
 	@SneakyThrows(Exception.class)
 	public void delete(int faq_no) {
 		repo.delete(faq_no);
