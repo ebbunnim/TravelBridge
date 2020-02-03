b79ef73230ec1810f123395472dcf993c92b7f36
신지영 <ebbunnim@gmail.com>
2020-02-03 15:54:39 +0900

    files까지 함

--- back-spring/src/main/java/com/pjt1/demo/model/service/FilesService.java
+++ back-spring/src/main/java/com/pjt1/demo/model/service/FilesService.java
@@ -5,9 +5,9 @@ import java.util.List;
 import com.pjt1.demo.model.dto.Files;
 
 public interface FilesService {
-	public Files search(int no);
+	public Files search(int files_no);
 	public List<Files> searchAll();
 	public void insert(Files Files);
 	public void update(Files Files);
-	public void delete(int no);
+	public void delete(int files_no);
 }
--- back-spring/src/main/java/com/pjt1/demo/model/service/FilesServiceImpl.java
+++ back-spring/src/main/java/com/pjt1/demo/model/service/FilesServiceImpl.java
@@ -13,32 +13,56 @@ public class FilesServiceImpl implements FilesService {
 	@Autowired 
 	private FilesService repo;
 	@Override
-	public Files search(int no) {
+	public Files search(int files_no ) {
 		// TODO Auto-generated method stub
+		try {
+			return repo.search(files_no);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 		return null;
 	}
 
 	@Override
 	public List<Files> searchAll() {
 		// TODO Auto-generated method stub
+		try {
+			return repo.searchAll();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 		return null;
 	}
 
 	@Override
 	public void insert(Files Files) {
 		// TODO Auto-generated method stub
+		try {
+			repo.insert(Files);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 
 	}
 
 	@Override
 	public void update(Files Files) {
 		// TODO Auto-generated method stub
-
+		try {
+			repo.update(Files);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
-	public void delete(int no) {
+	public void delete(int files_no ) {
 		// TODO Auto-generated method stub
+		try {
+			repo.delete(files_no);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 
 	}
 
