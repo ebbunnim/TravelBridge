67934f6a8e8f5e102b18f4d2f662848b86ee56db
김수민 <mytoutbon@gmail.com>
2020-02-11 13:20:37 +0900

    Finish City Service, Hotplace Service, Festival Servic
     - GET request on

--- front-vue/src/services/CityService.js
+++ front-vue/src/services/CityService.js
@@ -2,20 +2,30 @@ import Api from "./Api";
 
 class CityService {
   searchAll() {
-    return Api.get("/City");
+    return Api.get("/City")
+      .then(res => {
+        console.log(res.data.data);
+        return res.data.data;
+      })
+      .catch(e => console.log(e));
   }
   search(no) {
-    return Api.get(`/City/${no}`);
+    return Api.get(`/City/${no}`)
+      .then(res => {
+        console.log(res.data.data);
+        return res.data.data;
+      })
+      .catch(e => console.log(e));
   }
-  insert(city) {
-    return Api.post(`/City/`, city);
-  }
-  delete(no) {
-    return Api.delete(`/city/${no}`);
-  }
-  update(city) {
-    return Api.update(`/City`, city);
-  }
+  // insert(city) {
+  //   return Api.post(`/City/`, city);
+  // }
+  // delete(no) {
+  //   return Api.delete(`/city/${no}`);
+  // }
+  // update(city) {
+  //   return Api.update(`/City`, city);
+  // }
 }
 
 export default new CityService();
