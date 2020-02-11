c8bce4466ea4ac250946a5b075c736904747d76d
김수민 <mytoutbon@gmail.com>
2020-02-11 09:00:14 +0900

    Modify login-page views and main2search-page view

--- front-vue/src/components/two/Main2Search.vue
+++ front-vue/src/components/two/Main2Search.vue
@@ -1,13 +1,18 @@
 <template>
   <q-page padding="">
-    <div>
-      <h2>검색 컴포넌트다</h2>
-      <q-input v-model="search" type="search" hint="Search">
+    <div class="row justify-center q-my-xl q-py-xl">
+      <q-input
+        align="center"
+        class="col-xs-12 col-lg-6 self-center"
+        v-model="search"
+        type="search"
+        @input="onInput()"
+      >
         <template v-slot:prepend>
           <q-icon name="search" />
         </template>
-        <template v-slot:append>
-          <q-btn outline color="primary" label="SEARCH" @click="onSearchBtn(event.target.value)"/>
+        <template v-slot:after>
+          <q-btn outline color="grey" label="SEARCH" @click="onSearchBtn()" />
         </template>
       </q-input>
     </div>
@@ -22,8 +27,11 @@ export default {
     };
   },
   methods: {
+    onInput() {
+      // console.log(this.search)
+    },
     onSearchBtn() {
-      console.log(event.target.value)
+      console.log(this.search);
     }
   }
 };
