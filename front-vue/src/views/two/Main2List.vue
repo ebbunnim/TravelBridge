9e04eb3cf518ea85dc9a637b27f9df1e1baa1caa
김수민 <mytoutbon@gmail.com>
2020-02-17 18:04:01 +0900

    Modify Main2 page

--- front-vue/src/views/two/Main2List.vue
+++ front-vue/src/views/two/Main2List.vue
@@ -1,22 +1,22 @@
 <template>
   <q-page class="page">
     <div
-      class="row justify-center q-my-xl q-py-xl"
-      style="height: 550px; background: #f9f9f9"
+      class="row justify-center text-center q-my-xl q-py-xl"
+      style="height: 400px; background: #f9f9f9"
     >
       <q-select
-        bordered
-        class="col-1 q-mx-xs text-grey"
+        no-wrap
+        class="col-md-2 col-xs-4 text-h6 q-mx-sm q-px-lg"
         v-model="searchOption"
         :options="searchOptions"
       />
       <q-input
-        bordered
-        class="col-3 text-h6"
+        class="col-md-4 col-xs-6 text-h6"
         v-model="word"
-        type="text"
         :placeholder="[
-          searchOption === null ? '' : searchOption + '을 검색하세요'
+          (searchOption === null) | (searchOption === '전체')
+            ? ''
+            : searchOption + '을 검색하세요'
         ]"
       >
         <template v-slot:append>
@@ -30,59 +30,44 @@
         </template>
       </q-input>
     </div>
-    <div class>
-      <h4>이건 fval</h4>
-      {{ fval_list }}
-      <h4>이건 hp</h4>
-      {{ hp_list }}
-    </div>
-
-    <!-- <div class="row" style="height: 550px">
-      <div class="col text-h5"></div>
+    <div class="row justify-center q-mx-xl q-px-xl" style="height: 550px">
       <div
-        class="col"
-        v-for="i in hp_list.length > 4 * hpBtnCnt
-          ? 4 * hpBtnCnt
-          : hp_list.length"
+        class="col-lg-3 col-md-6 col-xm-12"
+        v-for="i in hp_list_length"
         :key="i"
       >
+        <!-- 카드가 들어가는 부분 -->
         <HotPlaceCard
+          class="q-ma-lg"
           :hp_name="hp_list[i - 1].hp_name"
           :hp_detail_adr="hp_list[i - 1].hp_detail_adr"
           :hp_tag="hp_list[i - 1].hp_tag"
+          :hp_no="hp_list[i - 1].hp_no"
         ></HotPlaceCard>
+        <!--  -->
       </div>
-    </div>
-    <div class="row" style="height: 550px; background: #f9f9f9">
-      <div class="col text-h5"></div>
-
-      <div
-        class="col"
-        v-for="i in fval_list.length > 4 * hpBtnCnt
-          ? 4 * fvalBtnCnt
-          : fval_list.length"
-        :key="i"
+      <q-btn
+        outline
+        square
+        class="col-xs-12 col-lg-10 q-ma-lg"
+        v-if="hpLoadMore"
+        @click="onSearchBtn()"
+        >더보기</q-btn
       >
-        <FestivalCard
-          :fval_name="fval_list[i - 1].fval_name"
-          :fval_detail_adr="fval_list[i - 1].fval_detail_adr"
-          :fval_tag="fval_list[i - 1].fval_tag"
-        ></FestivalCard>
-      </div>
-    </div>-->
+    </div>
   </q-page>
 </template>
 
 <script>
 import { mapState } from "vuex";
-// import HotPlaceCard from "@/views/two/HotPlaceCard.vue";
-// import FestivalCard from "@/views/two/FestivalCard.vue";
+import HotPlaceCard from "@/views/two/HotPlaceCard.vue";
+import FestivalCard from "@/views/two/FestivalCard.vue";
 
 export default {
-  // components: {
-  // HotPlaceCard,
-  // FestivalCard
-  // },
+  components: {
+    HotPlaceCard,
+    FestivalCard
+  },
   data() {
     return {
       hpBtnCnt: 1,
@@ -91,7 +76,7 @@ export default {
       hpLoadMore: false,
       fvalLoadMore: false,
       //
-      searchOption: null,
+      searchOption: "전체",
       searchOptions: ["전체", "제목", "내용", "태그", "도시"],
       searchOptionValue: {
         전체: "all",
@@ -103,25 +88,28 @@ export default {
       word: ""
     };
   },
-  computed: mapState({
-    hp_list: state => state.hotplace.hps,
-    fval_list: state => state.festival.fvals
-  }),
+  computed: {
+    ...mapState({
+      hp_list_length: state => state.hotplace.hp_list_length, // vuex 에서 데려옴
+      hp_list: state => state.hotplace.hps,
+      fval_list: state => state.festival.fvals
+    })
+  },
   methods: {
     onSearchBtn() {
       const payLoad = {
-        btnCnt: this.hpBtnCnt, // fval
+        btnCnt: this.hpBtnCnt + 1,
         searchOption: this.searchOptionValue[this.searchOption],
         word: this.word
       };
+      console.log("vue에서:", payLoad);
       this.$store.dispatch("hotplace/searchMoreHotplace", payLoad);
-      payLoad.btnCnt = this.fvalBtnCnt;
-      this.$store.dispatch("festival/searchMoreFestival", payLoad);
-    },
-    checkLoadMoreBtn() {
-      if (this.hp_list.length < 4) this.hpLoadMore = false;
-      if (this.fval_list.length < 4) this.fvalLoadMore = false;
     }
+    // checkLoadMoreBtn() {
+    //   if (4 * this.hpBtnCnt < this.hp_list_length <= 4 * (this.hpBtnCnt + 1)) {
+    //     this.hpLoadMore = true;
+    //   }
+    // }
   }
 };
 </script>
