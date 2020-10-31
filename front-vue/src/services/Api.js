69a3c647c2628679bebb97cdcf388c860c039528
ebbunnim <ebbunnim@gmail.com>
2020-10-31 13:41:07 +0900

    todo : auto-build dist + move dist to nginx conf

--- front-vue/src/services/Api.js
+++ front-vue/src/services/Api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://15.164.216.248:8080/TravelBridge-0.0.1-SNAPSHOT/",
-  // baseURL :"http://localhost:8081",
+  //baseURL: "http://15.164.216.248:8080/TravelBridge-0.0.1-SNAPSHOT/",
+  baseURL :"http://localhost:8082",
   withCredentials: false,
   headers: {
     Accept: "application/json",
